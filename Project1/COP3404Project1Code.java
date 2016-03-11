c34eab3c4e2d1d4a7e1b5c8eb062ef3ae63787f1
Matthew Boyette <Dyndrilliac@gmail.com>
2016-03-10 19:54:21 -0500

    Whitespace Formatting.

--- Project1/COP3404Project1Code.java
+++ Project1/COP3404Project1Code.java
@@ -3,8 +3,7 @@
  * Author: Matthew Boyette
  * Date: 2/11/2015
  * 
- * This program serves as the entry-point for Project 1.
- * See SimpleSymbolTable.java for the implementation.
+ * This program serves as the entry-point for Project 1. See SimpleSymbolTable.java for the implementation.
  */
 
 import api.util.sicxe.SimpleSymbolTable;
@@ -12,22 +11,21 @@ import api.util.stdlib.StdOut;
 
 public class COP3404Project1Code
 {
-	public static final void main(final String[] args)
-	{
-		SimpleSymbolTable[] programs = new SimpleSymbolTable[args.length];
-		
-		// Read in files as command-line arguments.
-		if (args.length > 0)
-		{
-			for (int i = 0; i < args.length; i++)
-			{
-				programs[i] = new SimpleSymbolTable(args[i]);
-			}
-		}
-		else
-		{
-			StdOut.print("Usage:\n\n\tCOP3404Project1Code [File1] [File2] [File#] ...\n\n" +
-				"Example:\n\n\tCOP3404Project1Code input test.txt source.asm\n\n");
-		}
-	}
+    public static final void main(final String[] args)
+    {
+        SimpleSymbolTable[] programs = new SimpleSymbolTable[args.length];
+        
+        // Read in files as command-line arguments.
+        if (args.length > 0)
+        {
+            for (int i = 0; i < args.length; i++)
+            {
+                programs[i] = new SimpleSymbolTable(args[i]);
+            }
+        }
+        else
+        {
+            StdOut.print("Usage:\n\n\tCOP3404Project1Code [File1] [File2] [File#] ...\n\n" + "Example:\n\n\tCOP3404Project1Code input test.txt source.asm\n\n");
+        }
+    }
 }
--- Project3&4/COP3404Project3Code.java
+++ Project3&4/COP3404Project3Code.java
@@ -3,8 +3,7 @@
  * Author: Matthew Boyette
  * Date: 3/27/2015
  * 
- * This program serves as the entry-point for Projects 3 & 4.
- * See SICXE_AssemblerProgram.java for the implementation.
+ * This program serves as the entry-point for Projects 3 & 4. See SICXE_AssemblerProgram.java for the implementation.
  */
 
 import api.util.sicxe.SICXE_AssemblerProgram;
@@ -13,31 +12,31 @@ import api.util.stdlib.StdOut;
 
 public class COP3404Project3Code
 {
-	public static final void main(final String[] args)
-	{
-		SICXE_AssemblerProgram[] programs = new SICXE_AssemblerProgram[Math.max(1, args.length)];
-		
-		// Read in file names given as command-line arguments.
-		if (args.length > 0)
-		{
-			for (int i = 0; i < args.length; i++)
-			{
-				programs[i] = new SICXE_AssemblerProgram(args[i]);
-			}
-		}
-		// Prompt the user for a file name if not given one.
-		else
-		{
-			StdOut.print("Enter source file name: ");
-			String fileName = StdIn.readString();
-			
-			if (fileName != null)
-			{
-				if (!fileName.isEmpty())
-				{
-					programs[0] = new SICXE_AssemblerProgram(fileName);
-				}
-			}
-		}
-	}
+    public static final void main(final String[] args)
+    {
+        SICXE_AssemblerProgram[] programs = new SICXE_AssemblerProgram[Math.max(1, args.length)];
+        
+        // Read in file names given as command-line arguments.
+        if (args.length > 0)
+        {
+            for (int i = 0; i < args.length; i++)
+            {
+                programs[i] = new SICXE_AssemblerProgram(args[i]);
+            }
+        }
+        // Prompt the user for a file name if not given one.
+        else
+        {
+            StdOut.print("Enter source file name: ");
+            String fileName = StdIn.readString();
+            
+            if (fileName != null)
+            {
+                if (!fileName.isEmpty())
+                {
+                    programs[0] = new SICXE_AssemblerProgram(fileName);
+                }
+            }
+        }
+    }
 }
